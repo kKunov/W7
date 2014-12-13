0639160b11ab6fcb77b11b9a196e95743cb2c949
Kokal <slim_shady121.2@abv.bg>
2014-12-13 15:43:21 +0200

    addin sites.txt and searcher now get sites from sites.txt

--- Search_Engine/searcher.py
+++ Search_Engine/searcher.py
@@ -1,10 +1,19 @@
 from searcher_class import Spider
 
 
+def search_more_than_one_site():
+    file = open('sites.txt', 'r')
+    sites = file.read().split('\n')
+    file.close()
+
+    for line in sites:
+        print(line)
+        site = Spider(line)
+        site.scan_website()
+
+
 def main():
-    website = input("Site:")
-    site = Spider(website)
-    site.scan_website()
+    search_more_than_one_site()
 
 
 if __name__ == '__main__':
